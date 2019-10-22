932b9e049fbe8f25ec876c0772a0351043045956
JoanManuelMarti <juanma887@gmail.com>
2019-10-22 23:28:51 +0200

    Version beta 1.0

--- src/hundirlaflota/HundirLaFlota.java
+++ src/hundirlaflota/HundirLaFlota.java
@@ -1,74 +1,90 @@
-
 package hundirlaflota;
 
 import java.util.Scanner;
 
-
-
 public class HundirLaFlota {
 
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       int[] movimiento=new int[2];
-       final int[] tablero={5,7};
-       int movimientos=0;
-       int numeroMovimientos=((tablero[0]*tablero[1])-30);
-       
-       System.out.println("Bienvenido al juego **HUNDIR LA FLOTA**");
-       System.out.println("INICIO DEL JUEGO");
-       tableroJugador tablero1=new tableroJugador(tablero);
-       tableroMaquina tablero2=new tableroMaquina(tablero1.GetArray());
-       tablero1.setPosicionBarco(tablero2.GetPosicionBarco());
-       
-       System.out.println(tablero2.GetPosicionBarco()[0]);
-       System.out.println(tablero2.GetPosicionBarco()[1]);
-       
-    while(tablero1.GetGanado()==false & movimientos<numeroMovimientos){
-        tablero1.dibujaTablero();
-        System.out.println();
-       
-        
-        
-        movimiento[0]=(tablero[0]+100);
+        Scanner sc = new Scanner(System.in);
+        int[] movimiento = new int[2];
+        final int[] tablero = {50,50};
+        int movimientos = 0;
+        int numeroMovimientos;
 
-        while(movimiento[0]>tablero[0]){
-            try{
-                System.out.println("Introduce fila");
-                movimiento[0]=Integer.parseInt(sc.nextLine());  
-    
-            } catch (Exception e){
-                System.out.println("Por favor introduce un numero del 1 al " + tablero[0]); 
+        System.out.println("Bienvenido al juego **HUNDIR LA FLOTA**");
+        System.out.println("INICIO DEL JUEGO");
+        
+        while ((tablero[0]<4 | tablero[0]>20) | (tablero[1]<4 | tablero[1]>20)) {
+            System.out.println("Introduce una medida de tablero de entre 4x4 a 20x20");
+            try {
+                System.out.println("Introduce el numero de filas");
+                tablero[0] = Integer.parseInt(sc.nextLine());
+                System.out.println("Introduce el numero de columnas");
+                tablero[1] = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Introduce una medida de tablero de entre 4x4 a 20x20");
             }
         }
-        
-      
-        
-        movimiento[1]=(tablero[1]+100);
-        while(movimiento[1]>tablero[1]){
-            try{
-                System.out.println("Introduce columna");
-                movimiento[1]=Integer.parseInt(sc.nextLine()); 
-              
-            } catch (Exception e){
-                System.out.println("Por favor introduce un numero del 1 al " + tablero[1]); 
+        numeroMovimientos=((tablero[0] * tablero[1]) - 10);
+        tableroJugador tablero1 = new tableroJugador(tablero);
+        tableroMaquina tablero2 = new tableroMaquina(tablero);
+        tablero1.setPosicionBarco(tablero2.GetPosicionBarco());
+
+        while (tablero1.GetGanado() == false & movimientos < numeroMovimientos) {
+            tablero1.dibujaTablero();
+            System.out.println();
+            System.out.println();
+            if (movimientos == 1) {
+                System.out.println("AGUA!!! Llevas " + movimientos + " movimiento realizado y te quedan " + (numeroMovimientos - movimientos) + " movimientos para finalizar el juego!!!");
+            } else if (movimientos > 1) {
+                System.out.println("AGUA!!! Llevas " + movimientos + " movimientos realizados y te quedan " + (numeroMovimientos - movimientos) + " movimientos para finalizar el juego!!!");
             }
+
+            do {
+                movimiento[0] = (tablero[0] + 100);
+                while (movimiento[0] > tablero[0] | movimiento[0] <= 0) {
+                    try {
+                        System.out.println();
+                        System.out.println("Introduce fila");
+                        movimiento[0] = Integer.parseInt(sc.nextLine());
+
+                    } catch (Exception e) {
+                        System.out.println("Por favor introduce un numero del 1 al " + tablero[0]);
+                    }
+                }
+
+                movimiento[1] = (tablero[1] + 100);
+                while (movimiento[1] > tablero[1] | movimiento[1] <= 0) {
+                    try {
+                        System.out.println("Introduce columna");
+                        movimiento[1] = Integer.parseInt(sc.nextLine());
+
+                    } catch (Exception e) {
+                        System.out.println("Por favor introduce un numero del 1 al " + tablero[1]);
+                    }
+                }
+                if (tablero1.GetArray()[movimiento[0]][movimiento[1]] == "[AGUA ]") {
+                    System.out.println();
+                    System.out.println("Cordenas ya introducidas anteriormente, por favor inseterte unas nuevas cordenadas.");
+                }
+            } while (tablero1.GetArray()[movimiento[0]][movimiento[1]] == "[AGUA ]");
+
+            tablero1.realizaMovimiento(movimiento);
+            movimientos++;
         }
-        tablero1.realizaMovimiento(movimiento);
-        movimientos++;
+
+        tablero1.dibujaTableroPerdedor();
+
+        System.out.println();
+        System.out.println();
+
+        if (tablero1.GetGanado() == false) {
+            System.out.println("No has encontrado el barco en: " + movimientos + " movimiento/s!, el barco estaba en la fila: " + tablero2.GetPosicionBarco()[0] + " y en la columna: " + tablero2.GetPosicionBarco()[1]);
+        } else {
+            System.out.println("Enhorabuena!!!, has encontrado el barco en " + movimientos + " movimiento/s!!");
+        }
+
+        System.out.println();
     }
-    
-    tablero1.dibujaTableroPerdedor();
-    
-    System.out.println();
-    System.out.println();
-    
-    if(tablero1.GetGanado()==false){
-        System.out.println("No has encontrado el barco en: " + movimientos + " movimiento/s!, el barco estaba en la fila: " + tablero2.GetPosicionBarco()[0] + " y en la columna: " + tablero2.GetPosicionBarco()[1]);
-    } else {
-        System.out.println("Enhorabuena!!!, has encontrado el barco en " + movimientos + " movimiento/s!!");
-    }
-    
-    System.out.println();
-    }
-    
+
 }
--- src/hundirlaflota/tableroMaquina.java
+++ src/hundirlaflota/tableroMaquina.java
@@ -5,20 +5,20 @@ package hundirlaflota;
 public class tableroMaquina {
     int[] b=new int[2];
     
-    public tableroMaquina(String[][] a){
-        b[0]=(int)(Math.random()*10);
-        b[1]=(int)(Math.random()*10);
+    public tableroMaquina(int[] a){
+        b[0]=(int)(Math.random()*a[0]);
+        b[1]=(int)(Math.random()*a[1]);
         
         if(b[0]==0){
           b[0]=1;  
-        } else if(b[0]>5){
-            b[0]=5;
+        } else if(b[0]>a[0]){
+            b[0]=a[0];
         }
         
         if(b[1]==0){
             b[1]=1;
-        } else if(b[1]>7){
-            b[1]=7;
+        } else if(b[1]>a[1]){
+            b[1]=a[1];
         }
         
     }
